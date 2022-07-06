aa57d9d62cdb6dbd4b97b6765d47244518a08442
ravi991 <raviranjancs4@gmail.com>
2022-07-06 14:58:30 -0400

    Add files via upload

---
+++ SearchClass.java
@@ -0,0 +1,33 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class SearchClass {
+	ChromeDriver driver;
+	@Given("I am on google page {string}")
+	public void i_am_on_google_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(string);
+	}
+
+	@When("I search = {string}")
+	public void i_search(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("q")).sendKeys(string);
+	    
+	}
+
+	@When("I click on google search button")
+	public void i_click_on_google_search_button() {
+	    // Write code here that turns the phrase above into concrete actions
+		 driver.findElement(By.name("btnK")).submit();
+	   
+	}
+
+}
